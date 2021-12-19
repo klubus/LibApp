4a0f90279e1ea401a7bb6aeb6f10cf3bb215d1d8
Krystian Kluba <klubakrystian@gmail.com>
2021-12-19 20:51:24 +0100

    Added MembershipTypes to database

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -8,7 +8,10 @@ namespace LibApp.Models
     public class Customer
     {
         public int Id { get; set; }
-
+        public bool HasNewsletterSubscribed { get; set; }
         public string Name { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+
     }
 }
